15f2ffeaf01385e1f912b2eac674ce5ea749458a
zhass <developer.kz@gmail.com>
2025-02-24 16:20:27 +0500

    Tasks

--- src/main/java/com/home/task/config/JdbcConfig.java
+++ src/main/java/com/home/task/config/JdbcConfig.java
@@ -8,8 +8,6 @@ import org.springframework.integration.jdbc.lock.DefaultLockRepository;
 import org.springframework.integration.jdbc.lock.JdbcLockRegistry;
 import org.springframework.integration.jdbc.lock.LockRepository;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 
@@ -31,9 +29,4 @@ public class JdbcConfig extends AbstractJdbcConfiguration {
     public NamedParameterJdbcTemplate namedParameterJdbcTemplate(DataSource dataSource) {
         return new NamedParameterJdbcTemplate(dataSource);
     }
-
-   /* @Bean
-    public DataSourceTransactionManager transactionManager(DataSource dataSource) {
-        return new DataSourceTransactionManager(dataSource);
-    }*/
 }
