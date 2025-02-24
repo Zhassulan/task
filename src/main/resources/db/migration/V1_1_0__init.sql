71365d120a9ca9bf86a3f98c1d568913736cf26f
zhass <developer.kz@gmail.com>
2025-02-24 22:47:12 +0500

    Tasks

--- src/main/resources/db/migration/V1_1_0__init.sql
+++ src/main/resources/db/migration/V1_1_0__init.sql
@@ -4,15 +4,16 @@ create table task_result (
     id serial PRIMARY KEY,
     request_id uuid not null,
     task_id int not null,
-    `min` int not null,
-    `max` int not null,
-    `count` int not null,
+    "min" int not null,
+    "max" int not null,
+    "count" int not null,
     result  integer[],
     successful boolean default false,
     message varchar(255),
     created TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-
+drop index if exists IDX_REQ_ID;
+drop index if exists IDX_SUCCESS;
 CREATE UNIQUE INDEX IDX_REQ_ID ON task_result(request_id);
-CREATE INDEX IDX_REQ_ID ON task_result(successful);
+CREATE INDEX IDX_SUCCESS ON task_result(successful);
